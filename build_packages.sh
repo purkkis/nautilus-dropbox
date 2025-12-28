cd97cc47935ec044fbcd3197ca40799acb5900a0
purkkis <joona@purkamo.me>
2025-12-28 09:04:33 +0200

    libnautilus-extension-4

--- build_packages.sh
+++ build_packages.sh
@@ -5,17 +5,17 @@ set -euxo pipefail
 rm -rf build
 
 # Build deb packages.
-for DISTRO in ubuntu debian debian_i386; do
-    docker build -t nautilus-dropbox/$DISTRO -f Dockerfile.$DISTRO .
-    docker run -u "$(id -u):$(id -g)" -v `pwd`:/src nautilus-dropbox/$DISTRO ./generate-deb.sh
-done
+# for DISTRO in ubuntu debian debian_i386; do
+#     docker build -t nautilus-dropbox/$DISTRO -f Dockerfile.$DISTRO .
+#     docker run -u "$(id -u):$(id -g)" -v `pwd`:/src nautilus-dropbox/$DISTRO ./generate-deb.sh
+# done
 
 # Build fedora rpm.
 docker build -t nautilus-dropbox/fedora -f Dockerfile.fedora .
-docker run -u "$(id -u):$(id -g)" -v `pwd`:/src nautilus-dropbox/fedora ./generate-rpm.sh
+docker run -u "$(id -u):$(id -g)" -v $(pwd):/src nautilus-dropbox/fedora ./generate-rpm.sh
 
 # Build the source tgz.
-make dropbox dist
-mkdir -p build/packages
-cp dropbox build/packages/dropbox.py
-cp nautilus-dropbox-*.tar.bz2 build/packages/
+# make dropbox dist
+# mkdir -p build/packages
+# cp dropbox build/packages/dropbox.py
+# cp nautilus-dropbox-*.tar.bz2 build/packages/
